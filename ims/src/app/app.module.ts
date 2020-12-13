28e1e6675de475a5aa8188af3a89205fcad8e837
吴毅斌 <wuyibin19850923@126.com>
2020-12-13 21:47:20 +0800

    submit pages login

--- ims/src/app/app.module.ts
+++ ims/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import zh from '@angular/common/locales/zh';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HashLocationStrategy } from '@angular/common';
+import { DevNgZorroAntdModule } from './ng-zorro-antd.module';
 
 @NgModule({
   declarations: [
@@ -22,10 +30,25 @@ import { HomeComponent } from './pages/home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
-    NzButtonModule
+    NzButtonModule,
+    NzFormModule,
+    NzIconModule,
+    NzGridModule,
+    DevNgZorroAntdModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: NZ_I18N,
+      useValue: zh_CN
+    },
+    {
+      provide: HashLocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
